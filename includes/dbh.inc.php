551c31736693efa36049f65667f2a9231ab14ab1
AshauranceHeavens <70837777+AshauranceHeavens@users.noreply.github.com>
2021-09-20 10:04:01 +0200

    Fixed some errors

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -9,13 +9,4 @@ $password = "";
   $pdo = new PDO($dsn, $username, $password);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// BETTER METHOD
-/*
-try{
-    $dsn = 'mysql:host='.$host.'; dbname='.$databaseName;
-    $pdo = new PDO($dsn, $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}catch(PDOException $error){
-    echo "error: " .$error->getMessage();
-}
-*/
+// CREATE TABLES BELOW
