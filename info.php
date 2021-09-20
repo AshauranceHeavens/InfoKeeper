5c532797649bc221a21651d9c9b01665c63e5516
AshauranceHeavens <70837777+AshauranceHeavens@users.noreply.github.com>
2021-09-20 09:50:23 +0200

    fixed some error

--- info.php
+++ info.php
@@ -1,94 +1,4 @@
 <?php
-
-//THE BELOW CODE IS THE CODE THE WORKS WHEN SEARCHING FOR A PERSON ON THE BADATABASE
-     //  require_once "header.php";
-     //
-     //  if(isset($_POST['submit-name-find']) && !empty($_POST['find-name'])){
-     //   require_once 'includes/dbh.inc.php';
-     //
-     //
-     //   $name = $_POST['find-name'];
-     //
-     //   $sql = "SELECT * FROM people WHERE name LIKE :name OR second_name LIKE :name  "; /*SEARCH FOR SECOND OR FIRSTNAME and if two rows have the same name only one shows*/
-     //   $stmt = $pdo->prepare($sql);
-     //
-     //   if($stmt){
-     //     $stmt->bindValue(':name', $name);
-     //     $stmt->bindValue(':name', $name);
-     //     $stmt->execute();
-     //     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-     //
-     //
-     //            if($results != null){
-     //              echo "<main>
-     //                    <h2> Mr. ".$_SESSION['NAME']."</h2>";
-     //              $rows = $results;
-     //              //foreach($rows as $row){
-     //                echo "
-     //                <div>
-     //                  <table>
-     //                    <tr>
-     //                      <th> ID  </th>
-     //                      <th> Name </th>
-     //                      <th> Second Name  </th>
-     //                      <th> Surname </th>
-     //
-     //                    </tr>";
-     //                    foreach($rows as $row){
-     //                    echo"
-     //                    <tr>
-     //                      <td>" .$row['id']. "</td>
-     //                      <td> ". $row['name'] ."</td>
-     //                      <td> " .$row['second_name']. "</td>
-     //                      <td> " . $row['surname'] ."</td>
-     //                      <td class='buttons'><div >
-     //                          <form method='post' action=# class='table-form'>
-     //                             <input type='hidden' name='id-view' value = ".$row['id'].">
-     //                             <input type='submit' name='view-person' value='View'>
-     //                          </form>
-     //
-     //                          <form method='post' action=# class='table-form'>
-     //                             <input type='hidden' name='id-update' value = ".$row['id'].">
-     //                             <input type='submit' name='Update-person' value='Update'>
-     //                          </form>
-     //
-     //                          <form method='post' action='includes/deletePerson.inc.php' class='table-form'>
-     //                             <input type='hidden' name='id-delete' value = ".$row['id'].">
-     //                             <input type='submit' name='delete-person' value='Delete'>
-     //                          </form>
-     //                      </div></td>
-     //                    </tr>
-     //                  ";
-     //              }
-     //             echo "</table>
-     //           </div>
-     //           </main>";
-     //            }else{
-     //              header("Location:../main.php?results=null");
-     //              exit();
-     //            }
-     //   }else{
-     //     header("Location: ../main.php?error=sqlerror");
-     //     exit();
-     //   }
-     //
-     // }else if(isset($_POST['find-name'])){
-     //
-     //       if(empty($_POST['find-name'])){
-     //         header("Location:../main.php?error=NameFieldIsEmpty");
-     //         exit();
-     //       }
-     //
-     //  }else{
-     //   header("Location: ../main.php");/*if submit button was not pressed*/
-     //   exit();
-     //  }
-
-//THE ABOVE CODE IS THE CODE THE WORKS WHEN SEARCHING FOR A PERSON ON THE BADATABASE
-
-
-
-//THE CODE BELOW IS FOR FINDING A PERSON ON THE DATABASE, IT IS BEING WOKED ON CURRENTLY
       require_once "header.php";
 
       // var_dump($_SESSION['array']);
@@ -146,11 +56,5 @@
        header("Location:main.php?error=SessionDoesn'tExist");
        exit();
      }
-
-
-
-
-//THE CODE ABOVE IS FOR FINDING A PERSON ON THE DATABASE, IT IS BEING WORKED ON CURRENTLY
-
 
    ?>
